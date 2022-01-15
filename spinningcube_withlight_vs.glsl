2963622fbd56c7ddaf546d523dbaeedfff1a6a53
Iván Barrientos Lema <ivan.barrientos.lema@gmail.com>
2022-01-15 18:59:31 +0100

    Code explanation

--- spinningcube_withlight_vs.glsl
+++ spinningcube_withlight_vs.glsl
@@ -17,6 +17,5 @@ void main() {
   gl_Position = projection * view * model * vec4(v_pos, 1.0f);
   frag_3Dpos = vec3(model * vec4(v_pos, 1.0));
   vs_normal = normalize(mat3(transpose(inverse(model))) * v_normal);
-  //vs_normal = normalize(normal_to_world * v_normal);
   tex_coord = v_tex;
 }
